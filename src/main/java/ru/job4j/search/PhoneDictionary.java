54400aa420ac94410e95404231d0e296fe400a7e
Ivan Kozhevnikov <xx71@mail.ru>
2020-12-16 21:12:54 +0300

    1. Телефонный справочник на базе ArrayList [#396066]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -10,13 +10,10 @@ public class PhoneDictionary {
     }
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
-        int i = 0;
         for (Person p : persons) {
-            if (persons.get(i).getSurname().contains(key) || persons.get(i).getName().contains(key) ||
-                    persons.get(i).getPhone().contains(key) || persons.get(i).getAddress().contains(key)) {
+            if (p.getSurname().contains(key) || p.getName().contains(key) ||
+                    p.getPhone().contains(key) || p.getAddress().contains(key)) {
                 result.add(p);
-                System.out.println(result.get(i).getSurname());
-                i++;
             }
         }
         return result;
