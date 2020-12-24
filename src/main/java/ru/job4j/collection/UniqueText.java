9ae2de45bd2b27a4cc05802c15cb2f27500a0849
Ivan Kozhevnikov <xx71@mail.ru>
2020-12-24 16:59:31 +0300

    2. Идентичные тексты [#396080]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String l : origin) {
+            check.add(l);
+        }
+        for (String t : text) {
+            if (!check.contains(t))
+                rsl = false;
+        }
+        return rsl;
+    }
+}
