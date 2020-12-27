769b9114d774e9c4ef4b04e3546e1ca095ab9102
Ivan Kozhevnikov <xx71@mail.ru>
2020-12-27 17:35:13 +0300

    0. Сортировка [#396064]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Objects;
+
 public class Item implements Comparable<Item> {
     private int id;
     private String name;
@@ -28,6 +30,18 @@ public class Item implements Comparable<Item> {
     @Override
     public int compareTo(Item another) {
         return Integer.compare(id, another.id);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return id == item.id && Objects.equals(name, item.name);
     }
 
     @Override
