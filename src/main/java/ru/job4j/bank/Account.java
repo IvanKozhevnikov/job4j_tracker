0d0c48c5c7b1d0c147371d2a25274e9cda202c54
Ivan Kozhevnikov <xx71@mail.ru>
2021-01-05 22:32:29 +0300

    3. Что такое JavaDoc [#396053]

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -2,31 +2,79 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель банковского счёта.
+ * Класс осуществляет доступ к полям requisite и balance и возможности их сравнения.
+ *
+ * @author IVAN KOZHEVNIKOV
+ * @version 1.0
+ */
 public class Account {
+
+    /**
+     * Через данное поле осуществляется доступ к номерам реквезитов счетов.
+     */
     private String requisite;
+
+    /**
+     * Через данное поле осуществляется доступ к сумме денежных средств
+     * находящихся на счетах.
+     */
     private double balance;
 
+    /**
+     * Метод принимает на вход реквизиты и сумму денежных средств.
+     * и после этого изменяет свойства класса записывая значения в переменные.
+     *
+     * @param requisite реквизиты счёта.
+     * @param balance сумма денежных средств счёта.
+     */
     public Account(String requisite, double balance) {
         this.requisite = requisite;
         this.balance = balance;
     }
 
+    /**
+     * Метод возвращает значение реквизитов счёта.
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * Метод принимает на вход реквизиты счёта.
+     * и после этого изменяет свойства класса записывая значения в переменную.
+     *
+     * @param requisite реквизиты счёта.
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * Метод возвращает значение суммы денежных средств счёта.
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Метод принимает на вход сумму денежных средств счёта.
+     * и после этого изменяет свойства класса записывая значения в переменную.
+     *
+     * @param balance сумма денежных средств счёта.
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Переопределение метода equals,
+     * после которого метод учитывает содержание полей,
+     * возвращая результат сравнения вводимого значения с содержанием поля.
+     *
+     * @param o переменная класса Object.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +87,11 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
+    /**
+     * Метод переопределяет метод hashCode,
+     * создавая низкую вероятность возникновения переменных с одинаковым адресом.
+     * Возращает этот медод хеш код переменной.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(requisite);
--- src/main/java/ru/job4j/bank/User.java
+++ src/main/java/ru/job4j/bank/User.java
@@ -2,31 +2,78 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель пользователя банка.
+ * Класс осуществляет доступ к полям passport и username и возможности их сравнения.
+ *
+ * @author IVAN KOZHEVNIKOV
+ * @version 1.0
+ */
 public class User {
+
+    /**
+     * Через данное поле осуществляется доступ к номеу паспорта пользователя.
+     */
     private String passport;
+
+    /**
+     * Через данное поле осуществляется доступ к имени пользователя.
+     */
     private String username;
 
+    /**
+     * Метод принимает на вход номер паспорта и имя пользователя.
+     * и после этого изменяет свойства класса записывая значения в переменные.
+     *
+     * @param passport номер паспорта пользователя.
+     * @param username имя пользователя.
+     */
     public User(String passport, String username) {
         this.passport = passport;
         this.username = username;
     }
 
+    /**
+     * Метод возвращает значение переменной номера паспорта пользователя.
+     */
     public String getPassport() {
         return passport;
     }
 
+    /**
+     * Метод принимает на вход номер паспорта пользователя.
+     * и после этого изменяет свойства класса записывая значения в переменную.
+     *
+     * @param passport номер паспорта пользователя.
+     */
     public void setPassport(String passport) {
         this.passport = passport;
     }
 
+    /**
+     * Метод возвращает значение переменной имя пользователя.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Метод принимает на вход имя пользователя.
+     * и после этого изменяет свойства класса записывая значения в переменную.
+     *
+     * @param username имя пользователя.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Переопределение метода equals,
+     * после которого метод учитывает содержание полей,
+     * возвращая результат сравнения вводимого значения с содержанием поля.
+     *
+     * @param o переменная класса Object.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +86,11 @@ public class User {
         return Objects.equals(passport, user.passport);
     }
 
+    /**
+     * Переопределение метода hashCode,
+     * создаёт низкую вероятность возникновения переменных с одинаковым адресом.
+     * Возращает этот метод хеш код переменной.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(passport);
