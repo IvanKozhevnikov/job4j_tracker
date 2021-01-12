bcdeb84f167a89ced6dc58ff044e809d60550acd
Ivan Kozhevnikov <xx71@mail.ru>
2021-01-13 01:16:02 +0300

    2.0. Встроенные функциональные интерфейсы. [#395892]

--- src/main/java/ru/job4j/lambda/SearchAtt.java
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -1,45 +1,38 @@
 package ru.job4j.lambda;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class SearchAtt {
 
-    private static Attachment filter(List<Attachment> list, Function<Attachment, Attachment> func) {
+    private static List<Attachment> filter(List<Attachment> list, Predicate<Attachment> func) {
         List<Attachment> rsl = new ArrayList<>();
-        for (Attachment attachment : list) {
-           // rsl = (List<Attachment>) func.apply(list.get(attachment.getSize(), attachment.getName()));
-
+        for (Attachment att : list) {
+            if (func.test(att)) {
+                rsl.add(att);
+                return rsl;
+            }
         }
-        return (Attachment) rsl;
+        return null;
     }
 
     public static List<Attachment> filterSize(List<Attachment> list) {
-        Function<Attachment, Attachment> func = new Function<Attachment, Attachment>() {
-            @Override
-            public Attachment apply(Attachment attachment) {
-                List<Attachment> rsl = new ArrayList<>();
-                if (attachment.getSize() > 100) {
-                    rsl.add(new Attachment(attachment.getName(), attachment.getSize()));
-                }
-                return (Attachment) rsl;
-            }
-        };
-        return (List<Attachment>) filter(list, func);
+        Predicate<Attachment> func = attachment -> attachment.getSize() > 100;
+        return filter(list, func);
     }
 
     public static List<Attachment> filterName(List<Attachment> list) {
-        Function<Attachment, Attachment> func = new Function<Attachment, Attachment>() {
-            @Override
-            public Attachment apply(Attachment attachment) {
-                List<Attachment> rsl = new ArrayList<>();
-                if (attachment.getName().contains("bug")) {
-                    rsl.add(new Attachment(attachment.getName(), attachment.getSize()));
-                }
-                return (Attachment) rsl;
-            }
-        };
-        return (List<Attachment>) filter(list, func);
+        Predicate<Attachment> func = attachment -> attachment.getName().contains("bug");
+        return filter(list, func);
+    }
+
+    public static void main(String[] args) {
+        List<Attachment> list = Collections.singletonList(
+                new Attachment("bug", 110));
+        System.out.println(filterSize(list));
+        System.out.println(filterName(list));
     }
 }
